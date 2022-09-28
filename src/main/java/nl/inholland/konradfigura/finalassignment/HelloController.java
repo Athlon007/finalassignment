98103f9c806b5b6ed9ce91289669c6fb9b4273bb
Athlon007 <aathlon@outlook.com>
2022-09-28 18:00:04 +0200

    Add login GUI

--- src/main/java/nl/inholland/konradfigura/finalassignment/HelloController.java
+++ src/main/java/nl/inholland/konradfigura/finalassignment/HelloController.java
@@ -1,14 +1,36 @@
 package nl.inholland.konradfigura.finalassignment;
 
+import javafx.beans.property.StringProperty;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Label lblWarning;
+
+    @FXML
+    private TextField txtUsername;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    protected void onLoginButtonClick(ActionEvent event) {
+        StringBuilder builder = new StringBuilder();
+        boolean valid = isPasswordValid(builder);
+
+        if (!valid) {
+            lblWarning.setText(builder.toString());
+        }
+    }
+
+    private boolean isPasswordValid(StringBuilder failReason) {
+        // TODO: Implement it :D
+        failReason.append("User does not exist.");
+        return false;
     }
 }
