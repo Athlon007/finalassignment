ec4bb4409c0e9f8f28a6185e3e46763bdaeb7c80
Athlon007 <aathlon@outlook.com>
2022-09-30 21:45:16 +0200

    Add collection

--- src/main/java/nl/inholland/konradfigura/finalassignment/Dashboard.java
+++ src/main/java/nl/inholland/konradfigura/finalassignment/Dashboard.java
@@ -5,11 +5,11 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class Dashboard implements Initializable {
@@ -28,6 +28,9 @@ public class Dashboard implements Initializable {
     @FXML
     private Label lblWelcome;
 
+    @FXML
+    private TableView tblItems;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         tabContainer.getSelectionModel().selectedItemProperty().addListener(
@@ -36,5 +39,30 @@ public class Dashboard implements Initializable {
                 }
         );
         tabContainer.getSelectionModel().select(tabLending);
+
+        // Initialize columns for tblItems.
+        //initItemsTableView();
+    }
+
+    private void initItemsTableView() {
+        TableColumn id = new TableColumn("Item Code");
+        // Small workaround, so the initial size of Item Code and Available headers is smaller.
+        id.setPrefWidth(100);
+        id.setMaxWidth(100);
+        id.setMinWidth(100);
+        TableColumn available = new TableColumn("Available");
+        available.setPrefWidth(100);
+        available.setMaxWidth(100);
+        available.setMinWidth(100);
+        TableColumn title = new TableColumn("Title");
+        TableColumn author = new TableColumn("Author");
+        tblItems.getColumns().addAll(id, available, title, author);
+
+        // Setting it back, so we can still resize Item Code and Available.
+        id.setMaxWidth(10000);
+        id.setMinWidth(1);
+        available.setMaxWidth(10000);
+        available.setMinWidth(1);
     }
 }
+
