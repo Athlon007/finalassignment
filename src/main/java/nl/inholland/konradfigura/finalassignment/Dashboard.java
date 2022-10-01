da0352c1be3f38887b4c89977258e759172361d9
Athlon007 <aathlon@outlook.com>
2022-10-01 23:35:10 +0200

    Add item and member panes

--- src/main/java/nl/inholland/konradfigura/finalassignment/Dashboard.java
+++ src/main/java/nl/inholland/konradfigura/finalassignment/Dashboard.java
@@ -3,10 +3,12 @@ package nl.inholland.konradfigura.finalassignment;
 import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -34,6 +36,12 @@ public class Dashboard implements Initializable {
     @FXML
     private TableView tblMembers;
 
+    @FXML
+    private AnchorPane paneAddMember;
+
+    @FXML
+    private AnchorPane paneAddItem;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         tabContainer.getSelectionModel().selectedItemProperty().addListener(
@@ -42,6 +50,28 @@ public class Dashboard implements Initializable {
                 }
         );
         tabContainer.getSelectionModel().select(tabLending);
+    }
+
+    @FXML
+    protected void onAddMemberClick(ActionEvent event) {
+        paneAddMember.setVisible(true);
+        // TODO: Clean text fields.
+    }
+
+    @FXML
+    protected void onCancelAddMemberClick(ActionEvent event) {
+        paneAddMember.setVisible(false);
+    }
+
+    @FXML
+    protected void onAddItemClick(ActionEvent event) {
+        paneAddItem.setVisible(true);
+        // TODO: Clean text fields.
+    }
+
+    @FXML
+    protected void onCancelAddItemClick(ActionEvent event) {
+        paneAddItem.setVisible(false);
     }
 }
 
